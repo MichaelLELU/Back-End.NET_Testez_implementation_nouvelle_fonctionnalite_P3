329b7afdd0f7065d1820bf62f205daa50af9e945
Michaël Lelu <michael.lelu@vexinsoft.fr>
2025-04-23 18:50:01 +0200

    Ajout de la validation des produits et des tests associés
    Création de la classe `ProductServiceValidationTests` pour tester la validation des produits. Ajout de la méthode `CheckProduct` dans `ProductService.cs` pour valider les modèles de produits avec des annotations de données. Mise à jour des propriétés `Price` et `Stock` dans `ProductViewModel.cs` avec des annotations de validation. Introduction de nouveaux fichiers de stub pour simuler les dépendances dans les tests, facilitant ainsi les tests unitaires du `ProductService`.

---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/Stubs/CartStub.cs
@@ -0,0 +1,33 @@
+﻿using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests.Stubs
+{
+    internal class CartStub : ICart
+    {
+        public void AddItem(Product product, int quantity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double GetAverageValue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double GetTotalValue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveLine(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/Stubs/StringLocalizerStub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Localization;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using System.Collections.Generic;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests.Stubs
+{
+    internal class StringLocalizerStub : IStringLocalizer<ProductService>
+    {
+        public LocalizedString this[string name] => throw new System.NotImplementedException();
+
+        public LocalizedString this[string name, params object[] arguments] => throw new System.NotImplementedException();
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
